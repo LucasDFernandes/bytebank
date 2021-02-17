4781d4ae4966a2a2e67d2bff318d80f0f0e77660
ice_lfernandes <ice_lfernandes@uolinc.com>
2021-02-17 00:04:34 -0300

    Declarando variaveis

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    print("Hello World")
+    println("Hello World")
+
+    val titular = "Lucas Fernandes"
+    val numeroConta = 755
+    var saldo = 0.0
+    saldo = 100.0
+
+    println("Titular é $titular")
+    println("Numero da Conta é $numeroConta")
+    println("Saldo é $saldo")
 }
